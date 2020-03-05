4ab95d815a356546379a240b2115dc5da79774c7
sahid25 <ashrafsahid25@gmail.com>
2020-03-05 15:21:23 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// Falsy value 
+// ""
+// 0
+// undefined
+// false 
+//null
+// NaN
+
+// Truthy value 
+//" "
+// "0"
+//true 
+
+
+
+let name = 0;
+// console.log(name);
+if (name || name == 0) {
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
