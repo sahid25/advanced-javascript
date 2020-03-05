1ddd81f5792011e5e7fd38445041aabba8505b80
sahid25 <ashrafsahid25@gmail.com>
2020-03-05 16:43:56 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,30 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+const result1 = add(12, 23);
+console.log(result1);
+
+function add(name1, name2) {
+  console.log(name1, name2);
+  return;
+}
+const result2 = add(33);
+console.log(result2);
+
+const result3 = {
+  name: "ashraf ahmed sahid",
+  phone: 34343434
+};
+console.log(result3.address);
+
+const result4 = undefined;
+console.log(result4);
+
+const age = [23, 24, 25, 26];
+console.log(age[11]); //show me 11 number element 
+
+const show = null;
+console.log(show)
