6ec960303a92f35f615f0560b78c02c139654a30
sahid25 <ashrafsahid25@gmail.com>
2020-03-05 20:28:56 +0600

    onek kichu shiklam

---
+++ map-filter.js
@@ -0,0 +1,29 @@
+const number = [2, 3, 4, 5, 6, 7, 8, 9];
+// const output = [];
+
+// for (let i = 0; i < number.length; i++) {
+//   const element = number[i];
+//   const result = element * element;
+//   output.push(result);
+// }
+// console.log(output);
+
+// function square(element) {
+//   return element = element * element;
+// }
+
+// const square = element => element * element; //another way function
+// const square = x => x * x; //another way function 
+
+
+// const result = number.map(function (element) {
+//   return element * element;
+// })
+
+// const result = number.map(x => x * x);
+// console.log(result);
+
+// const bigger = number.filter(x => x < 3);
+const theFind = number.find(x => x > 6);
+console.log(theFind);
+// console.log(bigger);
---
+++ object-proparty.js
@@ -0,0 +1,34 @@
+const Students = [{
+    Id: 44,
+    UserName: "Acer"
+  },
+  {
+    Id: 45,
+    UserName: "Hp"
+  },
+  {
+    Id: 46,
+    UserName: "Dell"
+  },
+  {
+    Id: 50,
+    UserName: "Apple"
+  },
+];
+const ShowMe = [];
+
+// for (let i = 0; i < Students.length; i++) {
+//   const element = Students[i];
+//   const result = element.UserName;
+//   ShowMe.push(result);
+// }
+// console.log(ShowMe);
+
+// const ids = Students.map(x => x.Id);
+// const name = Students.map(x => x.UserName);
+
+// const idFilter = Students.filter(x => x.Id > 40);
+// console.log(idFilter);
+
+// const idFinding = Students.find(x => x.Id > 40);
+// console.log(idFinding);
